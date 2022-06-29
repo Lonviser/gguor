c67ea06ce670abff53adc23461cd567172534915
Lonviser <47rex47@mail.ru>
2022-06-29 15:34:24 +0300

    moore

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,5 +8,97 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-    
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView:1,
+        // spaceBetween: 0,
+        slideToClickedSlide: true,
+    },
+       425: {
+        slidesPerView: 2,
+        spaceBetween: 0,
+       slideToClickedSlide: true,
+     },
+     570: {
+        slidesPerView: 3,
+        spaceBetween: 0,
+        slideToClickedSlide: true,
+    },
+      1050: {
+          slidesPerView:4,
+          // spaceBetween: 0,
+          slideToClickedSlide: true,
+      },
+      // when window width is >= 480px
+      960: {
+          slidesPerView: 4,
+          // spaceBetween: 0,
+          slideToClickedSlide: true,
+      },          
+      1320: {
+          slidesPerView: 6,
+          // spaceBetween: 0,
+          slideToClickedSlide: true,
+      },
+    //   1440: {
+    //     slidesPerView: 4,
+    //     // spaceBetween: 0,
+    //     slideToClickedSlide: true,
+    // }
+  }
   });
+
+  const partners = new Swiper('.s-partners', {
+    slidesPerView: 4,
+    spaceBetween: 10,
+    freeMode: true,
+    loop: true,   
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.partners__next',
+    prevEl: '.partners__prev',
+  },
+
+  breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView:1,
+          // spaceBetween: 0,
+          slideToClickedSlide: true,
+      },
+      //   425: {
+      //     slidesPerView: 2,
+      //     // spaceBetween: 0,
+      //     slideToClickedSlide: true,
+      // },
+      // 570: {
+      //   slidesPerView: 3,
+      //   // spaceBetween: 0,
+      //   slideToClickedSlide: true,
+      // },
+        620: {
+            slidesPerView:2,
+            // spaceBetween: 0,
+            slideToClickedSlide: true,
+        },
+        // when window width is >= 480px
+        960: {
+            slidesPerView: 3,
+            // spaceBetween: 0,
+            slideToClickedSlide: true,
+        },          
+        1320: {
+            slidesPerView: 4,
+            // spaceBetween: 0,
+            slideToClickedSlide: true,
+        },
+      //   1440: {
+      //     slidesPerView: 4,
+      //     // spaceBetween: 0,
+      //     slideToClickedSlide: true,
+      // }
+    }
+}); 
+
